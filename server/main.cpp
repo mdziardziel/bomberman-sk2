6ae2881c19d56d1726e0c42ee7b31dfb234049d7
mdziardziel <mdziardziel@gmail.com>
2019-01-01 14:00:27 +0100

    add config file

--- server/main.cpp
+++ server/main.cpp
@@ -1,9 +1,14 @@
 // 'Hello World!' program 
  
 #include <iostream>
+#include "BombermanServerConfig.hpp"
  
 int main()
 {
   std::cout << "Hello World!" << std::endl;
+  fprintf(stdout,"%s Version %d.%d\n",
+          argv[0],
+          BombermanServer_VERSION_MAJOR,
+          BombermanServer_VERSION_MINOR);
   return 0;
 }
