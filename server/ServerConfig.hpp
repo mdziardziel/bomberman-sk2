5a5e43028c6ce136c4ad0840dfb2103fdaad63c0
mdziardziel <mdziardziel@gmail.com>
2019-01-01 16:55:26 +0100

    fix gitignore

--- server/ServerConfig.hpp
+++
@@ -1,3 +0,0 @@
-// the configured options and settings for BombermanServer
-#define BombermanServer_VERSION_MAJOR @BombermanServer_VERSION_MAJOR@
-#define BombermanServer_VERSION_MINOR @BombermanServer_VERSION_MINOR@
