6edeeca709bbecf8d5d951a1185bdbaa285bcd7b
mdziardziel <mdziardziel@gmail.com>
2019-01-01 17:31:39 +0100

    add Client

---
+++ client/ClientConfig.hpp
@@ -0,0 +1,3 @@
+// the configured options and settings for BombermanClient
+#define BombermanClient_VERSION_MAJOR 1
+#define BombermanClient_VERSION_MINOR 0
--- client/main.cpp
+++ client/main.cpp
@@ -1,9 +1,12 @@
 // 'Hello World!' program 
  
 #include <iostream>
+#include "ClientConfig.hpp"
  
 int main()
 {
   std::cout << "Hello World!" << std::endl;
+  std::cout << BombermanClient_VERSION_MAJOR << std::endl;
+  std::cout << BombermanClient_VERSION_MINOR << std::endl;
   return 0;
 }
